e5e24cfe81d1c5eb5c714a0479b723b43127f9b0
Arjun <simplegamerofficial97@gmail.com>
2024-09-30 00:06:16 +0530

    Modified JS and CSS

--- todo.js
+++ todo.js
@@ -3,6 +3,10 @@ const itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getIt
 
 console.log(itemsArray);
 
+window.onload = function(){
+  displayItems();
+}
+
 document.querySelector('.add-btn').addEventListener('click', () => {
   const item = document.querySelector('.input-area');
   createItem(item);  //create item
@@ -26,13 +30,10 @@ function displayItems(){
       <i class="fa-solid fa-trash delete-btn"></i>
     </div></div>`;
   }
-
-  //let todoWrapper = document.querySelector('.todo-wrapper').
-  //todoWrapper.appendChild();
-  document.querySelector('.todo-div').innerHTML = items
+  document.querySelector('.todo-div').innerHTML = items;
   activateDeleteListeners();
   activateEditListeners();
-  activateMarkListeners();
+  activateCheckMarkListeners();
 }
 
 function activateDeleteListeners(){
@@ -48,80 +49,25 @@ function deleteItem(index){
   location.reload();
 }
 
-
 function activateEditListeners(){
   const editBtns = document.querySelectorAll('.edit-btn');
   const textArea = document.querySelectorAll('.todo-text');
   editBtns.forEach((eb, index) => {
     eb.addEventListener('click', () => {
       textArea[index].disabled = false;
+      textArea[index].focus();
     });
   });
 }
 
-function activateMarkListeners(){
-
-}
-
-
-window.onload = function(){
-  displayItems();
-}
-
-
-
-
-
-
-
-//earlier code
-/*function addItem(){
-  const inputText = document.querySelector(".input-area");
-  //console.log(inputText);
-  //console.log(inputText.value);
-  
-  
-  //storeInLocal();
-  todoDiv.innerHTML = 
-    `<textarea name="textarea" class="todo-text" disabled>${inputText.value}</textarea>
-    <div class="todo-icons">
-      <i class="fa-solid fa-check check-mark"></i>
-      <i class="fa-solid fa-pen-to-square edit"></i>
-      <i class="fa-solid fa-trash delete"></i>
-    </div>`;  
-  bottomContainer.appendChild(todoDiv);  
-  inputText.value = "";  
-
-  //container holding the todo items is bottomContainer 
-  // Add a single event listener to the bottomContainer (event delegation)
-  bottomContainer.addEventListener('click', function(event) {
-    // Get the clicked element
-    const clickedIcon = event.target;
-
-  // tocheck whick icon is clicked and action accordingly
-  if (clickedIcon.classList.contains('check-mark')) {
-    // Handle check mark click
-    console.log("Check icon clicked");
-    const todoDiv = clickedIcon.closest('.todo-div');
-    todoDiv.classList.toggle('completed'); // Toggle completed state
-    const todoText = clickedIcon.closest('.todo-div').querySelector('.todo-text');
-    todoText.disabled = true;
-
-  } else if (clickedIcon.classList.contains('edit')) {
-    // Handle edit icon click
-    console.log("Edit icon clicked");
-    const todoText = clickedIcon.closest('.todo-div').querySelector(".todo-text");
-    todoText.disabled = false; 
-    // Toggle between editable and non-editable
-    todoText.focus();
-
-  } else if (clickedIcon.classList.contains('delete')) {
-    // Handle delete icon click
-    console.log("Delete icon clicked");
-    const todoDiv = clickedIcon.closest('.todo-div');
-    todoDiv.remove(); // Remove the todo item
-  }
+function activateCheckMarkListeners(){
+  const markBtns = document.querySelectorAll('.check-mark-btn');
+  const todoDiv = document.querySelectorAll('.todo-list');
+  markBtns.forEach((mb, index) => {
+    mb.addEventListener('click', () =>{
+      todoDiv[index].classList.toggle('completed');
+    });
   });
-
 }
-*/
+
+
